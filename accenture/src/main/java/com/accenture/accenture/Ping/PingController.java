fe6056221e0098245df971c5cdeae761fd694eae
Wallace Martins <martinsswallace@gmail.com>
2024-05-03 13:52:24 -0300

    testando

--- accenture/src/main/java/com/accenture/accenture/Ping/PingController.java
+++ accenture/src/main/java/com/accenture/accenture/Ping/PingController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @RestController
 @RequestMapping(value = "ping")
 public class PingController {
@@ -13,5 +11,10 @@ public class PingController {
     @GetMapping
     public String ping(){
         return "pong";
+    }
+
+    @GetMapping
+    public String ping2(){
+        return "pong2";
     }
 }
